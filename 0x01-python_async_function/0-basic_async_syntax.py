2c59c14d99a3e4d7aac1e0987e28d99d44657858
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-06 14:57:31 +0100

    Create 0-basic_async_syntax.py

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+A module tat implements coroutines
+"""
+__all__ = ['wait_random']
+import random
+import asyncio
+
+
+def wait_random(max_delay=10):
+  ran = random.uniform(0, max_delay)
+  await asyncio.sleep(ran)
+  return ran
