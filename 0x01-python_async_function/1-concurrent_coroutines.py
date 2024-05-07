dc06b7a23f35ae640e071d7189b72b64c6e0bb07
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-07 21:21:43 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,7 +14,10 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     A function that execute multiple coroutines
     at the same time with async
     """
+    result = []
     delays = [await wait_random(max_delay) for _ in range(n)]
 
-    delays.sort()
-    return delays
+    for delay in asyncio.as_completed(delays):
+        result.append(await delay)
+
+    return result
