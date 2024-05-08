286b1d2ac16e1e07c8aab9bfad249a5388d34f75
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-08 09:01:41 +0100

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,8 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """
+    A function that generates through async
+    """
     tasks = [i async for i in async_generator()]
     return tasks
