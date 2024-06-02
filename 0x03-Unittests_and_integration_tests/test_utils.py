c5e661dbb8db4b86478fca128df4e5c3c3333090
Abasiekeme Etim <abasiekemeetim20@gmail.com>
2024-06-02 12:44:53 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -26,11 +26,14 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand({
+    @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
-    })
+    ])
     def test_access_nested_map_exception(self, nested_map, path):
         """ Error test"""
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
+
+if __name__ == "__main__":
+    unittest.main()
